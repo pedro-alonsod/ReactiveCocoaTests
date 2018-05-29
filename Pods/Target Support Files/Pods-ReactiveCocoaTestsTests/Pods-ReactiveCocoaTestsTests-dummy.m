f39e55f3a1b0ef3437b8f33db19cec98c2ea7b02
PedroA <pedro.alonsod@gmail.com>
2018-05-29 16:11:25 -0500

    First commit

---
+++ Pods/Target Support Files/Pods-ReactiveCocoaTestsTests/Pods-ReactiveCocoaTestsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactiveCocoaTestsTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactiveCocoaTestsTests
+@end
--- ReactiveCocoaTests/ViewController.swift
+++ ReactiveCocoaTests/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import ReactiveCocoa
+import ReactiveSwift
+import Result
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lbTextInputs: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textArea: UITextView!
+    @IBOutlet weak var btnReactive: UIButton!
+    @IBOutlet weak var lbButton: UILabel!
+    @IBOutlet weak var segmentedController: UISegmentedControl!
+    @IBOutlet weak var lbSegmented: UILabel!
+    @IBOutlet weak var swReactive: UISwitch!
+    @IBOutlet weak var lbSwitch: UILabel!
+    @IBOutlet weak var activityController: UIActivityIndicatorView!
+    @IBOutlet weak var sldReactive: UISlider!
+    @IBOutlet weak var prgssReactive: UIProgressView!
+    @IBOutlet weak var stpReactive: UIStepper!
+    @IBOutlet weak var lbStepper: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        self.lbTextInputs.reactive.text <~ self.lbTextInputs.reactive.text
+        
+         self.textField.reactive.continuousTextValues.map
+            {
+                print($0 ?? "", "count: \($0!.count)")
+                self.lbTextInputs.text = "\($0 ?? "") count: \($0!.count)"
+                return $0 ?? 0
+            }.observeValues {
+                print($0 , "count: ")
+        }
+        
+        self.textArea.reactive.continuousTextValues.map({
+            print($0, "Count: ", $0?.count ?? "")
+            self.lbTextInputs.text = "\($0 ?? "") count: \($0!.count)"
+            return $0 ?? "-1"
+        }).observeValues {
+            print($0)
+        }
     }
 
     override func didReceiveMemoryWarning() {
